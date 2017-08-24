e8019a45084f44db0cb8b1af68c9897a1cf916ed
陈飞(视频.211389) <feichen211389@sohu-inc.com>
2017-08-24 15:10:04 +0800

    finish

--- src/leetcode/p4.java
+++ src/leetcode/p4.java
@@ -8,14 +8,13 @@ import java.util.Random;
 public class p4 {
 
     public static void main(String[] args){
-        int[] nums1 = {};
-        int[] nums2 = {};
+        int[] nums1 = {3};
+        int[] nums2 = {1,2};
         p4 ans = new p4();
         System.out.println(ans.findMedianSortedArrays(nums1, nums2));
     }
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        double ans = 0.0;
 
         int m = nums1.length;
         int n = nums2.length;
@@ -28,16 +27,65 @@ public class p4 {
             nums1 = nums2;
             nums2 = t_a;
         }
-        Random random = new Random();
-        int i = random.nextInt(m);// index of nums1
+        int i = 0;
         int j = 0;//index of nums2
-
+        /* （m + n)/2 is another choice， be careful that if（m+n)/2 is choosed,
+         * min_right will be the middle case when m+n is odd, since middle=(m + n)/2   length(left_part) <= length(right_part)
+         */
+        int middle = (m + n + 1) / 2;
         boolean odd = (m + n)%2 == 1 ? true : false;
-        int append = odd ? -1 : 0;
+        int imin = 0;
+        int imax = m;
+        int max_left = 0;
+        int min_right = 0;
         while(true){
-            j = (m + n + append) / 2 - i;
+            //i + j = middle
+            //i point to leftest element of nums1's right part
+            i = (imin + imax)/2;
+            //j point to leftest element of nums2's right part
+            j = middle - i;
+
+            if(i > 0 && nums1[i -1] > nums2[j]){
+                imax = i - 1;
+            }else if(i < m && nums1[i] < nums2[j - 1]){
+                imin = i + 1;
+            }else{
+
+
+                if(i == 0){
+                    max_left = nums2[j - 1];
+                }else if(j == 0){
+                    max_left = nums1[i - 1];
+                }else {
+                    max_left = max(nums1[i - 1], nums2[j - 1]);
+                }
+
+                if(odd){
+                    return max_left;
+                }
+                if(i == m){
+                    min_right = nums2[j];
+                }else if(j == n){
+                    min_right = nums1[i];
+                }else {
+                    min_right = min(nums1[i], nums2[j]);
+                }
+                return (double)(min_right + max_left)/2;
+            }
         }
+    }
 
 
+    public int min(int a, int b){
+        if(a < b){
+            return a;
+        }
+        return b;
+    }
+    public int max(int a, int b){
+        if(a < b){
+            return b;
+        }
+        return a;
     }
 }
